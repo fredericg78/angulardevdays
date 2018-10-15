0e14ea86dd0ce327b913666bec13250b56b5adcc
Frédéric Gindre <frederic.gindre@orange.com>
2018-10-15 15:10:45 +0200

    step9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -12,12 +12,15 @@ import { FooterComponent } from './footer/footer.component';
 import { ConfigService } from './config.service';
 import { HeaderComponent } from './header/header.component';
 import { UserService } from './user.service';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
+import { RoutingModule } from './routing/routing.module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgbCollapseModule, CustomMaterialModule, AngularFontAwesomeModule ],
-  declarations: [ AppComponent, FooterComponent, HeaderComponent ],
+  imports:      [ BrowserModule, FormsModule, NgbCollapseModule, CustomMaterialModule, AngularFontAwesomeModule, ReactiveFormsModule, RoutingModule ],
+  declarations: [ AppComponent, FooterComponent, HeaderComponent, LoginComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ConfigService, UserService]
 })
